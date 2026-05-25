9fac76221ab77e41d303ad6534afa38c4c8b6733
Eric J. Meier <ericmeier@live.com>
2026-05-25 16:33:50 -0600

    moved getFieldDeck and harvest functions to the global script and just call them in both lib/playerBoard2 and lib/playerBoard3. Had to slightly modify harvest since it is being used in both the 3 field and 2 field board.

--- lib/playerBoard3.lua
+++ lib/playerBoard3.lua
@@ -1,6 +1,3 @@
-local discardPosition = {-5,0,0}
-local discardDropPosition = {discardPosition[1],discardPosition[2]+3,discardPosition[3]}
-
 function onLoad()
     -- Create button on the object this script is attached to
     self.createButton({
@@ -33,150 +30,13 @@ function onLoad()
 end
 
 function Harvest1()
-    harvest(1)
+    Global.call("harvest", {fieldIndex = 1, selfObj = self})
 end
 
 function Harvest2()
-    harvest(2)
+    Global.call("harvest", {fieldIndex = 2, selfObj = self})
 end
 
 function Harvest3()
-    harvest(3)
-end
-
-function getFieldDeck(position)
-    local fieldDeck
-    local beanName
-    local beanCount
-    local hitList = Physics.cast({
-        origin       = position,
-        type         = 2,
-        size         = {0.1,0.1,0.1},
-        debug        = false,
-        max_distance = 0,
-        direction    = {0,1,0}
-    })
-    for _, obj in ipairs(hitList) do
-        if obj.hit_object.tag == 'Deck' then
-            fieldDeck = obj.hit_object
-            beanName = fieldDeck.getObjects()[1].name
-            beanCount = fieldDeck.getQuantity()
-        end
-        if obj.hit_object.tag == 'Card' then
-            if obj.hit_object.getName() ~= 'playerBoard2' and obj.hit_object.getName() ~= 'playerBoard3' then
-                fieldDeck = obj.hit_object
-                beanName = fieldDeck.getName()
-                beanCount = 1
-            end
-        end
-    end
-    return fieldDeck, beanName, beanCount
-end
-
-function harvest(fieldIndex)
-    local fieldPosition = self.positionToWorld(self.getSnapPoints()[fieldIndex].position)
-    local fieldDeck
-    local beanName
-    local beanCount
-    fieldDeck, beanName, beanCount = getFieldDeck(fieldPosition)
-    if beanName == 'garden' and beanCount >= 3 then
-        broadcastToAll('Uwe is pleased')
-        self.setCustomObject({
-            face = "https://steamusercontent-a.akamaihd.net/ugc/12325834521653936125/36AC6D60E9BD32FA806F332A4279FC981BAD586A/", 
-            back = "https://steamusercontent-a.akamaihd.net/ugc/12325834521653936125/36AC6D60E9BD32FA806F332A4279FC981BAD586A/"
-        })
-    elseif beanName == 'blue' and beanCount >= 10 then
-        broadcastToAll('I;m thinking about thos Beans')
-        self.setCustomObject({
-            face = 'https://steamusercontent-a.akamaihd.net/ugc/14236327910096653218/DB19CA44E6A2391B2AA6794C7B0A2D6FB0DC7583/',
-            back = 'https://steamusercontent-a.akamaihd.net/ugc/14236327910096653218/DB19CA44E6A2391B2AA6794C7B0A2D6FB0DC7583/'
-        })
-    end
-    local coins = 0
-    local coinPosition = self.positionToWorld(self.getSnapPoints()[4].position)
-    local coinDropPosition = {coinPosition[1],coinPosition[2]+2,coinPosition[3]}
-    local discard = Global.call('getDiscard')
-    local value
-
-    local gardenValue = {{2,2},{3,3}}
-    local blueValue = {{4,1},{6,2},{8,3},{10,4}}
-    local stinkValue = {{3,1},{5,2},{7,3},{8,4}}
-    local greenValue = {{3,1},{5,2},{6,3},{7,4}}
-    local waxValue = {{4,1},{7,2},{9,3},{11,4}}
-    local blackeyedValue = {{2,1},{4,2},{5,3},{6,4}}
-    local chiliValue = {{3,1},{6,2},{8,3},{9,4}}
-    local soyValue = {{2,1},{4,2},{6,3},{7,4}}
-    local redValue = {{2,1},{3,2},{4,3},{5,4}}
-    local coffeeValue = {{4,1},{7,2},{10,3},{12,4}}
-    local cocoaValue = {{2,1},{3,3},{4,4}}
-    local fieldValue = {{2,'field'},{3,3}}
-
-    if beanName == 'garden' then
-        value = gardenValue
-    elseif beanName == 'blue' then
-        value = blueValue
-    elseif beanName == 'stink' then
-        value = stinkValue
-    elseif beanName == 'green' then
-        value = greenValue
-    elseif beanName == 'wax' then
-        value = waxValue
-    elseif beanName == 'blackeyed' then
-        value = blackeyedValue
-    elseif beanName == 'chili' then
-        value = chiliValue
-    elseif beanName == 'soy' then
-        value = soyValue
-    elseif beanName == 'red' then
-        value = redValue
-    elseif beanName == 'coffee' then
-        value = coffeeValue
-    elseif beanName == 'cocoa' then
-        value = cocoaValue
-    elseif beanName == 'field' then
-        value = fieldValue
-    elseif beanName == nil then
-        value = {{0,0}}
-    end
-
-    for _, doodoo in ipairs(value) do
-        if beanCount == nil then
-        elseif beanCount >= doodoo[1] then
-            coins = doodoo[2]
-        end
-    end
-    if coins == 'field' then
-        self.setState(2)
-        coins = 0
-    end
-    if fieldDeck == nil then
-        broadcastToAll("Y\'ain\'t got no beans!")
-    elseif fieldDeck.tag == 'Deck' then
-        for i, card in ipairs(fieldDeck.getObjects()) do
-            if i <= coins then
-                if i < beanCount then
-                    local poopoo = fieldDeck.takeObject()
-                    poopoo.setPositionSmooth(coinDropPosition,false)
-                    poopoo.setRotation({-180,0,0})
-                elseif i == beanCount then
-                    fieldDeck.remainder.setPositionSmooth(coinDropPosition,false)
-                    fieldDeck.remainder.setRotation({-180,0,0})
-                end
-            elseif i > coins then
-                if i < beanCount then
-                    local poopoo = fieldDeck.takeObject()
-                    poopoo.setPositionSmooth(discardDropPosition,false)
-                elseif i == beanCount then
-                    fieldDeck.remainder.setPositionSmooth(discardDropPosition,false)
-                end
-            end
-        end
-    elseif fieldDeck.tag == 'Card' then
-        if discard then
-            fieldDeck.setPosition({fieldPosition[1],fieldPosition[2]+3, fieldPosition[3]})
-            discard.putObject(fieldDeck)
-        else
-            fieldDeck.setPositionSmooth(discardDropPosition,false)
-        end   
-    end
+    Global.call("harvest", {fieldIndex = 3, selfObj = self})
 end
